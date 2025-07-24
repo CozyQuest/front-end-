7adaf79db2a2d1c4dfd7e1a0cbc2bbd4fde1b654
Ahmed Waleed Abdeen <ahmedwalleed15@gmail.com>
2025-07-24 18:51:14 +0300

    Add routing configuration for home and add-property components

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('./components/home/home/home').then(m => m.Home),
+  },
+  {
+    path: 'add-property',
+    loadComponent: () => import('./components/add-property/add-property').then(m => m.AddProperty),
+  }
+];
