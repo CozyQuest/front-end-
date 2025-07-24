bec794b6764ced48406bda82f93f32fff8debfc5
Nancy EL Sherbiny <103130605+NancELSherbiny@users.noreply.github.com>
2025-07-24 19:00:14 +0300

    property list page

--- front-end/src/app/app.ts
+++ front-end/src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PropertyList } from "./components/properties/property-list/property-list";
+import { PropertyCard } from './components/properties/property-list/property-card/property-card';
+import { FilterComponent } from './components/properties/property-list/filter-card/filter-card';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ PropertyList, CommonModule, ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- front-end/src/app/components/properties/property-list/property-list.ts
+++ front-end/src/app/components/properties/property-list/property-list.ts
@@ -2,14 +2,16 @@ import { Component } from '@angular/core';
 import { FilterComponent } from "./filter-card/filter-card";
 import { PropertyCard } from "./property-card/property-card";
 import { Apartment } from '../../../core/interfaces/apartment.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-property-list',
   standalone: true,
-  imports: [FilterComponent, PropertyCard],
+  imports: [FilterComponent, PropertyCard, CommonModule],
   templateUrl: './property-list.html'
 })
 export class PropertyList {
+  heroBackgroundImage = 'url(assets/images/01.jpg)';
   apartments: Apartment[] = [
     {
       id: 1,
@@ -19,7 +21,7 @@ export class PropertyList {
       area: 45,
       beds: 1,
       baths: 1,
-      imageUrl: 'https://via.placeholder.com/300x200?text=Zamalek+Studio',
+      imageUrl: 'https://shreethemes.in/hously/landing/assets/images/property/1.jpg',
       rating: 4.2,
       reviewCount: 23
     },
@@ -31,7 +33,7 @@ export class PropertyList {
       area: 120,
       beds: 3,
       baths: 2,
-      imageUrl: 'https://via.placeholder.com/300x200?text=New+Cairo+Luxury',
+      imageUrl: 'https://shreethemes.in/hously/landing/assets/images/property/2.jpg',
       rating: 4.7,
       reviewCount: 58
     },
@@ -43,7 +45,7 @@ export class PropertyList {
       area: 70,
       beds: 2,
       baths: 1,
-      imageUrl: 'https://via.placeholder.com/300x200?text=Alex+Flat',
+      imageUrl: 'https://shreethemes.in/hously/landing/assets/images/property/3.jpg',
       rating: 3.9,
       reviewCount: 12
     }
