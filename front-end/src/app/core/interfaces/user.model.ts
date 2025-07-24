afb5bfc8af21eda988c29145bf082968de74e4ac
Rodina ElFeky <rodina.elfeky228@gmail.com>
2025-07-24 17:18:38 +0300

    Interface Updates

---
+++ front-end/src/app/core/interfaces/user.model.ts
@@ -0,0 +1,10 @@
+export interface User {
+  id: number;
+  fname: string;
+  lname: string;
+  email: string;
+  phone: string;
+  profilePicUrl: string;
+  coverPicture?: string; 
+  bio?: string;
+}
