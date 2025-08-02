5d2d04ce8c11df57c0f0e22ff0bced88d5193c41
Ahmed-Aseel <ahmedaseel155@gmail.com>
2025-08-02 14:08:09 +0300

    feat(top-properties): show top 5 properties ordered by average rating

--- front-end/src/app/components/dashboard/top-properties/top-properties.ts
+++ front-end/src/app/components/dashboard/top-properties/top-properties.ts
@@ -1,49 +1,24 @@
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { TopPropertyService } from '../../../core/services/top-property.service';
+import { TopProperty } from '../../../core/interfaces/top-property.model';
 
 @Component({
-    selector: 'app-top-properties',
-    imports: [CommonModule],
-    templateUrl: './top-properties.html',
-    styleUrl: './top-properties.css'
+  selector: 'app-top-properties',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './top-properties.html',
+  styleUrl: './top-properties.css',
 })
-export class TopProperties {
-    properties = [
-        {
-            img: 'assets/images/property/1.jpg',
-            name: 'House',
-            location: 'Baton Rouge, USA',
-            change: '-11%',
-            isDown: true
-        },
-        {
-            img: 'assets/images/property/2.jpg',
-            name: 'House',
-            location: 'Baton Rouge, USA',
-            change: '+20%',
-            isDown: false
-        },
-        {
-            img: 'assets/images/property/3.jpg',
-            name: 'House',
-            location: 'Baton Rouge, USA',
-            change: '+24%',
-            isDown: false
-        },
-        {
-            img: 'assets/images/property/4.jpg',
-            name: 'House',
-            location: 'Baton Rouge, USA',
-            change: '+21%',
-            isDown: false
-        },
-        {
-            img: 'assets/images/property/5.jpg',
-            name: 'House',
-            location: 'Baton Rouge, USA',
-            change: '+45%',
-            isDown: false
-        },
-    ];
+export class TopProperties implements OnInit {
+  properties: TopProperty[] = [];
 
+  constructor(private topPropertyService: TopPropertyService) {}
+
+  ngOnInit() {
+    this.topPropertyService.getTopProperties().subscribe({
+      next: (res) => (this.properties = res),
+      error: (err) => console.error('Failed to load top properties', err)
+    });
+  }
 }
