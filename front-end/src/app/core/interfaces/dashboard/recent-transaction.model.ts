a5a153764b113093dbe18fb69d83434bdb29b99c
Ahmed-Aseel <ahmedaseel155@gmail.com>
2025-08-06 15:48:40 +0300

    refactor: adjust Transaction interface and template to match updated DTO
    - Updated user and property details per new DTO structure
    - Ensured links and data render correctly with modified interface

--- front-end/src/app/core/interfaces/dashboard/recent-transaction.model.ts
+++ front-end/src/app/core/interfaces/dashboard/recent-transaction.model.ts
@@ -1,9 +1,12 @@
 export interface RecentTransaction {
+  propertyId: number;
   img: string;
   title: string;
   location: string;
-  guestName: string;
+  hostId: string;
   hostName: string;
+  guestId: string;
+  guestName: string;
   price: string;
   date: string;
 }
