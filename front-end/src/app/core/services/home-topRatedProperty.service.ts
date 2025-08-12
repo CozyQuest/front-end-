d6e153e086d31045bf71e1aea6b869e2eea91a5a
Ahmed Waleed <ahmedwalleed15@gmail.com>
2025-08-12 13:34:15 +0300

    fix: update API URL to use environment variable and enhance carousel card styles

--- front-end/src/app/core/services/home-topRatedProperty.service.ts
+++ front-end/src/app/core/services/home-topRatedProperty.service.ts
@@ -2,10 +2,11 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { IPropertyCard } from '../interfaces/recomendedProperty';
 import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class HomeTopRatedPropertyService {
-  private readonly apiUrl = 'https://localhost:7279/api/property/top-rated';
+  private readonly apiUrl = environment.apiUrl+'/api/property/top-rated';
 
   constructor(private http: HttpClient) {}
 
